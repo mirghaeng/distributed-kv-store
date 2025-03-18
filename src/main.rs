589c8004f32ac8913caae149c29fab80b94e4897
Miranda Eng <mirandaengcodes@gmail.com>
2025-03-17 19:55:57 -0700

    feat: add key-value REST API endpoints

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #[macro_use] extern crate rocket;
 
 mod models;
-use models::{Value, StoreGetResponse};
+use models::{Value, StoreGetResponse, StorePutResponse, StoreDeleteResponse};
 
 use rocket::State;
 use rocket::http::Status;
@@ -9,9 +9,11 @@ use rocket::response::status;
 use rocket::serde::json::Json;
 
 use std::collections::HashMap;
-use std::sync::RwLock;
+use std::sync::Mutex;
 
-type Store = RwLock<HashMap<String, String>>;
+// A key-value store with causal consistency typically involves more writes than reads
+// Mutex for frequent writes, RwLock for read-heavy workloads
+type StoreType = Mutex<HashMap<String, String>>;
 
 #[get("/")]
 fn index() -> &'static str {
@@ -19,23 +21,100 @@ fn index() -> &'static str {
 }
 
 #[get("/<key>")]
-fn get_key(key: String) -> String {
-    format!("get on /key-value-store/{}", key)
+fn get_key(store: &State<StoreType>, key: String) -> status::Custom<Json<StoreGetResponse>> {
+    let store = store.lock().unwrap();
+    
+    // Case: Key does not exist, Return: 404 Not found
+    if !store.contains_key(&key) {
+        return status::Custom(Status::NotFound, Json(StoreGetResponse {
+            does_exist: false,
+            error: Some(String::from("Key does not exist")),
+            message: Some(String::from("Error in GET")),
+            value: None
+        }))
+    }
+
+    // Case: Key exists, Return: 200 OK
+    status::Custom(Status::Ok, Json(StoreGetResponse {
+        does_exist: true,
+        error: None,
+        message: Some(String::from("Retrieved successfully")),
+        value: store.get(&key).map(|s| s.into()),
+    }))
 }
 
-#[put("/<key>", format="application/json", data="<value>")]
-fn put_key(key: String, value: Json<Value>) -> String {
-    format!("put on /key-value-store/{} for value {}", key, value.value)
+#[put("/<key>", format="application/json", data="<val_op>")]
+fn put_key(store: &State<StoreType>, key: String, val_op: Option<Json<Value>>) -> status::Custom<Json<StorePutResponse>> {
+    let mut store = store.lock().unwrap();
+
+    match val_op {
+        
+        // Case: Value not specified, Return: 400 Bad Request
+        None => status::Custom(Status::BadRequest, Json(StorePutResponse {
+            message: String::from("Error in PUT"),
+            replaced: None,
+            error: Some(String::from("Value is missing")),
+        })),
+        Some(value) => {
+
+            // Case: Key length is greater than 50, Return 400 Bad Request
+            if key.len() > 50 {
+                status::Custom(Status::BadRequest, Json(StorePutResponse {
+                    message: String::from("Error in PUT"),
+                    replaced: None,
+                    error: Some(String::from("Key is too long")),
+                }))
+
+            // Case: Key exists, value specified, Return 200 OK
+            } else if store.contains_key(&key) {
+                store.insert(key, value.value.clone());
+                status::Custom(Status::Ok, Json(StorePutResponse {
+                    message: String::from("Updated successfully"),
+                    replaced: Some(true),
+                    error: None,
+                }))
+
+            // Case: Key does not exist, Return: 201 Created
+            } else {
+                store.insert(key, value.value.clone());
+                status::Custom(Status::Created, Json(StorePutResponse {
+                    message: String::from("Added successfully"),
+                    replaced: Some(false),
+                    error: None,
+                }))
+            }
+        }
+    }
 }
 
 #[delete("/<key>")]
-fn del_key(key: String) -> String {
-    format!("delete on /key-value-store/{}", key)
+fn del_key(store: &State<StoreType>, key: String) -> status::Custom<Json<StoreDeleteResponse>> {
+    let mut store = store.lock().unwrap();
+
+    // Case: Key does not exist, Return: 404 Not Found
+    if !store.contains_key(&key) {
+        return status::Custom(Status::NotFound, Json(StoreDeleteResponse {
+            does_exist: false,
+            error: Some(String::from("Key does not exist")),
+            message: Some(String::from("Error in DELETE")),
+            value: None,
+        }))
+    }
+
+    store.remove(&key);
+
+    // Case: Key exists, Return: 200 OK
+    status::Custom(Status::Ok, Json(StoreDeleteResponse {
+        does_exist: true,
+        error: None,
+        message: Some(String::from("Deleted successfully")),
+        value: None,
+    }))
 }
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-    .manage(RwLock::new(HashMap::<String, String>::new()))
+    .manage(Mutex::new(HashMap::<String, String>::new()))
     .mount("/key-value-store", routes![index, get_key, put_key, del_key])
 }
--- src/models/mod.rs
+++ src/models/mod.rs
@@ -16,8 +16,38 @@ pub struct StoreGetResponse {
     #[serde(skip_serializing_if = "Option::is_none")]
     pub error: Option<String>,
 
-    /// Optional message about the request result
+    pub message: Option<String>,
+
+    /// The value associated with the key (if it exists)
     #[serde(skip_serializing_if = "Option::is_none")]
+    pub value: Option<String>,
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct StorePutResponse {
+    pub message: String,
+
+    /// Optional error message if request fails
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub replaced: Option<bool>,
+
+    /// Optional error message if request fails
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub error: Option<String>,
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct StoreDeleteResponse {
+
+    /// Indicates whether the key exists in the store
+    pub does_exist: bool,
+
+    /// Optional error message if request fails
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub error: Option<String>,
+
     pub message: Option<String>,
 
     /// The value associated with the key (if it exists)
